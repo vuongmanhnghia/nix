87069d4cad138b0b05a47ce51b127396479b49d9
Nagih <vuongmanhnghia@gmail.com>
2025-08-20 07:08:48 +0700

    Laptop

--- home/shared/hypr/modules/environment.nix
+++ home/shared/hypr/modules/environment.nix
@@ -21,26 +21,6 @@
         "QT_SCALE_FACTOR,1"
         "QT_QPA_PLATFORMTHEME,qt5ct"
         "QT_QPA_PLATFORMTHEME,qt6ct"
-
-        # === CURSOR THEME ===
-        "XCURSOR_THEME,apple-cursor"
-        "XCURSOR_SIZE,24"
-        "HYPRCURSOR_THEME,apple-cursor"
-        "HYPRCURSOR_SIZE,24"
-
-        # ############ Wayland #############
-        "ELECTRON_OZONE_PLATFORM_HINT,auto"
-
-        # ############ Themes #############
-        "QT_QPA_PLATFORM,wayland"
-        "QT_QPA_PLATFORMTHEME,kde"
-        "XDG_MENU_PREFIX,plasma-"
-
-        # ############ Terminal application #########
-        "TERMINAL,kitty"
-
-        # ######## Virtual envrionment #########
-        "ILLOGICAL_IMPULSE_VIRTUAL_ENV, ~/.local/state/quickshell/.venv"
       ];
     };
   };
