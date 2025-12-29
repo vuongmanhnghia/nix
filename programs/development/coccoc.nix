a990017daf83aab67878809292af27f411e3d9fd
Nagih <vuongmanhnghia@gmail.com>
2025-12-30 00:53:43 +0700

    app(coccoc): Update build Coc Coc Browser

--- programs/development/coccoc.nix
+++ programs/development/coccoc.nix
@@ -1,61 +1,32 @@
-{
-  lib,
-  stdenv,
-  fetchurl,
-  dpkg,
-  autoPatchelfHook,
-  makeWrapper,
-  wrapGAppsHook3,
-  alsa-lib,
-  at-spi2-atk,
-  at-spi2-core,
-  atk,
-  cairo,
-  cups,
-  curl,
-  dbus,
-  expat,
-  fontconfig,
-  freetype,
-  gdk-pixbuf,
-  glib,
-  gtk3,
-  libdrm,
-  libnotify,
-  libpulseaudio,
-  libuuid,
-  libxkbcommon,
-  mesa,
-  nspr,
-  nss,
-  pango,
-  pipewire,
-  systemd,
-  xdg-utils,
-  xorg,
-  wayland,
-  snappy,
-  libva,
-  vulkan-loader,
-}:
+{ pkgs ? import <nixpkgs> {} }:
 
-stdenv.mkDerivation rec {
+let
   pname = "coccoc-browser";
   version = "141.0.7390.132-1";
+  
+  coccoc-src = pkgs.stdenv.mkDerivation {
+    name = "${pname}-source";
+    src = pkgs.fetchurl {
+      url = "https://browser-linux.coccoc.com/deb/pool/main/coccoc-browser-stable_${version}_amd64.deb";
+      sha256 = "0diqr1jvxxmfl32aa8d643b45v8irxjv6hra89ms50bc9c71rsi1"; 
+    };
+    
+    nativeBuildInputs = [ pkgs.dpkg ];
+    
+    unpackPhase = ''
+      dpkg-deb --fsys-tarfile $src | tar -x --no-same-permissions --no-same-owner
+    '';
 
-  src = fetchurl {
-    url = "https://browser-linux.coccoc.com/deb/pool/main/coccoc-browser-stable_${version}_amd64.deb";
-    sha256 = "0diqr1jvxxmfl32aa8d643b45v8irxjv6hra89ms50bc9c71rsi1"; # Thay bằng hash thực
+    installPhase = ''
+      mkdir -p $out
+      cp -r . $out
+    '';
   };
 
-  nativeBuildInputs = [
-    dpkg
-    autoPatchelfHook
-    makeWrapper
-    wrapGAppsHook3
-  ];
+in pkgs.buildFHSEnv {
+  name = "coccoc-browser";
 
-  buildInputs = [
+  targetPkgs = pkgs: with pkgs; [
     alsa-lib
     at-spi2-atk
     at-spi2-core
@@ -70,20 +41,30 @@ stdenv.mkDerivation rec {
     gdk-pixbuf
     glib
     gtk3
+    gtk4
+    
+    # Graphics & Hardware
     libdrm
+    libgbm          # THÊM libgbm trực tiếp
+    libglvnd
     libnotify
     libpulseaudio
     libuuid
-    libva
     libxkbcommon
     mesa
     nspr
     nss
     pango
+    pciutils
     pipewire
     snappy
+    systemd
+    udev
     vulkan-loader
     wayland
+    xdg-utils
+    
+    # X11
     xorg.libX11
     xorg.libXScrnSaver
     xorg.libXcomposite
@@ -97,79 +78,47 @@ stdenv.mkDerivation rec {
     xorg.libXtst
     xorg.libxcb
     xorg.libxshmfence
+    
+    # Fonts & Misc
+    liberation_ttf
+    wget
   ];
 
-  runtimeDependencies = [
-    systemd
+  multiPkgs = pkgs: with pkgs; [
+    mesa
+    libdrm
+    libgbm          # THÊM vào multiPkgs cũng
   ];
 
-  # Bỏ qua Qt shim libraries - chúng là optional cho file dialogs
-  autoPatchelfIgnoreMissingDeps = [
-    "libQt5Core.so.5"
-    "libQt5Gui.so.5"
-    "libQt5Widgets.so.5"
-    "libQt6Core.so.6"
-    "libQt6Gui.so.6"
-    "libQt6Widgets.so.6"
-  ];
-
-  unpackPhase = ''
-    runHook preUnpack
-    dpkg-deb --fsys-tarfile $src | tar -x --no-same-permissions --no-same-owner
-    runHook postUnpack
+  runScript = ''
+    #!${pkgs.bash}/bin/bash
+    exec ${coccoc-src}/opt/coccoc/browser/browser \
+      --disable-setuid-sandbox \
+      --ozone-platform-hint=auto \
+      --enable-features=WaylandWindowDecorations \
+      "$@"
   '';
 
-  installPhase = ''
-    runHook preInstall
-
-    mkdir -p $out/opt
-    cp -r opt/coccoc $out/opt/
-
-    # Copy share directory
-    mkdir -p $out/share
-    cp -r usr/share/* $out/share/
-
-    # Xóa chrome-sandbox (sẽ dùng namespace sandbox)
-    rm -f $out/opt/coccoc/browser/chrome-sandbox
-
-    # Xóa Qt shim files (optional, dùng cho native file dialogs)
-    # Browser vẫn hoạt động bình thường với GTK dialogs
-    rm -f $out/opt/coccoc/browser/libqt5_shim.so
-    rm -f $out/opt/coccoc/browser/libqt6_shim.so
-
-    # Binary chính là ./opt/coccoc/browser/browser
-    mkdir -p $out/bin
-    makeWrapper $out/opt/coccoc/browser/browser $out/bin/coccoc-browser \
-      --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath buildInputs}" \
-      --suffix PATH : ${lib.makeBinPath [ xdg-utils ]} \
-      --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}"
-
-    # Tạo symlink cho compatibility
-    ln -s $out/bin/coccoc-browser $out/bin/coccoc-browser-stable
-
-    # Sửa .desktop file
+  extraInstallCommands = ''
+    mkdir -p $out/share/applications
+    cp ${coccoc-src}/usr/share/applications/coccoc-browser.desktop $out/share/applications/
+    
     substituteInPlace $out/share/applications/coccoc-browser.desktop \
       --replace-fail "/usr/bin/coccoc-browser-stable" "$out/bin/coccoc-browser"
 
-    # Symlink icons
     for size in 16 24 32 48 64 128 256; do
-      if [ -f "$out/opt/coccoc/browser/product_logo_$size.png" ]; then
+      src_icon="${coccoc-src}/opt/coccoc/browser/product_logo_$size.png"
+      if [ -f "$src_icon" ]; then
         mkdir -p $out/share/icons/hicolor/''${size}x''${size}/apps
-        ln -s $out/opt/coccoc/browser/product_logo_$size.png \
-          $out/share/icons/hicolor/''${size}x''${size}/apps/coccoc-browser.png
+        ln -sf "$src_icon" $out/share/icons/hicolor/''${size}x''${size}/apps/coccoc-browser.png
       fi
     done
-
-    runHook postInstall
   '';
 
-  meta = with lib; {
-    description = "Cốc Cốc Browser - Trình duyệt web của Việt Nam";
+  meta = {
+    description = "Cốc Cốc Browser (FHS Environment)";
     homepage = "https://coccoc.com";
-    license = licenses.unfree;
-    maintainers = [ ];
+    license = pkgs.lib.licenses.unfree;
     platforms = [ "x86_64-linux" ];
-    sourceProvenance = with sourceTypes; [ binaryNativeCode ];
-    mainProgram = "coccoc-browser";
   };
 }
