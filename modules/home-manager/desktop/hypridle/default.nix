6650435b4c1b2571853ba224e709a5119c7b5f20
Nagih <vuongmanhnghia@gmail.com>
2026-02-07 01:45:08 +0700

    feat(hypridle): Update all hypridle config

--- modules/home-manager/desktop/hypridle/default.nix
+++ modules/home-manager/desktop/hypridle/default.nix
@@ -1,47 +1,8 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, end-4-dots, ... }:
 
-let
-  lock_cmd = "hyprctl dispatch global quickshell:lock & pidof qs quickshell hyprlock || hyprlock";
-  suspend_cmd = "systemctl suspend || loginctl suspend";
-in
 {
-  services.hypridle = {
-    enable = true;
-    settings = {
-      general = {
-        lock_cmd = "${lock_cmd}";
-        before_sleep_cmd = "loginctl lock-session";
-        after_sleep_cmd = "hyprctl dispatch global quickshell:lockFocus";
-        inhibit_sleep = 3;
-      };
+  services.hypridle.enable = true;
 
-      listener = [
-        # === LOCKSCREEN ===
-        {
-          timeout = 300;                                # 5min
-          on-timeout = "loginctl lock-session";         # lock screen when timeout has passed
-        }
-
-        # === TURN OFF SCREEN ===
-        {
-          timeout = 600;                                                      # 5.5min
-          on-timeout = "hyprctl dispatch dpms off";                          # screen off when timeout has passed
-          on-resume = "hyprctl dispatch dpms on";        # screen on when activity is detected after timeout has fired.
-        }
-
-        # === SUSPEND ===
-        {
-          timeout = 900;                               # 30min
-          on-timeout = "${suspend_cmd}";             # suspend pc
-        }
-        
-        # === MONITOR DIM ===
-        {
-          timeout = 300;                                # 5min.
-          on-timeout = "brightnessctl -s set 10";       # set monitor backlight to minimum, avoid 0 on OLED monitor.
-          on-resume = "brightnessctl -r";               # monitor backlight restore.
-        }
-      ];
-    };
-  };
+  xdg.configFile."hypr/hypridle.conf".text = 
+    builtins.readFile "${end-4-dots}/dots/.config/hypr/hypridle.conf";
 }
